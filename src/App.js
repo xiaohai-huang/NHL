236242125e79f79290f61442359215a40371ac13
xiaohai-huang <jeromehuang18@gmail.com>
2022-06-22 12:37:22 +1000

    add React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<p>Home Page</p>} />
+        <Route path="game-grid" element={<GameGrid />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/GameGrid/GameGrid.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./GameGrid.module.css"; // Import css modules stylesheet as styles
+
+function GameGrid() {
+  return <div>GameGrid</div>;
+}
+
+export default GameGrid;
