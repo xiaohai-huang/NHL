fcb5ae3273fe7b237f3682081b25d960945b502c
xiaohai-huang <jeromehuang18@gmail.com>
2022-07-02 16:15:28 +1000

    feat: adjust GameScores based on Back-2-Back games

--- src/utils/calcGameScore.ts
+++ src/utils/calcGameScore.ts
@@ -1,6 +1,5 @@
+import { DAYS, TeamRowData } from "../components/GameGrid/TeamRow";
 import getPython from "./initPython";
-
-// http://localhost:3000/pythonScripts/my_script.py
 
 let runtime: any = null;
 export async function initGameScoreEnv() {
@@ -25,14 +24,83 @@ export default async function calcGameScore(
     `get_game_scores("${homeTeam}","${awayTeam}")`
   );
   const parsed = JSON.parse(result) as [number, number, number];
-  console.log({ homeTeam, awayTeam, scores: parsed });
 
   return parsed[1];
 }
 
-export function handleBackToBackGameScore(gameScores: number | null[]) {
-  return [];
+/**
+ * Test if the day is the 2d day of a back-to-back.
+ * @param gameScores A list of GameScores
+ * @param day The index of the day
+ * @returns true if the `day` is 2d day of a back-to-back. Otherwise, false.
+ */
+export function isBackToBack(gameScores: (number | null)[], day: number) {
+  if (day <= 0) return false;
+  const playedYesterday = gameScores[day - 1] !== null;
+  const playedToday = gameScores[day] !== null;
+
+  if (!playedToday) return false;
+  if (playedYesterday && playedToday) return true;
+  return false;
 }
+
+/**
+ * Convert a TeamRowData to a list of GameScores.
+ * @param row A team's stats for a week
+ * @returns A list of GameScores. e.g., [null, 0.3, null, 0.3, 6.4, null, 2.7]
+ */
+export function convertTeamRowToGameScores(row: TeamRowData) {
+  const scores: (number | null)[] = [];
+  DAYS.forEach((day, i) => {
+    const score = row[day]?.score;
+    scores[i] = score !== undefined ? score : null;
+  });
+  return scores;
+}
+
+/**
+ *
+ * Adjust the **GameScore** if a team plays 2x in 2 nights (called a back to back), the gameScore is multiplied by `dilutedFactor` for the second game.
+ *
+ * If a team also plays a team that is playing game 2/2 in a back to back, no handicap is placed
+ *
+ * Higher the game Score, the better a chance a team has to win.
+ *
+ * Directly operates on the argument.
+ *
+ * @param teams The whole league
+ * @param dilutedFactor The penalty term for the 2d game.
+ */
+export function adjustBackToBackGames(
+  teams: TeamRowData[],
+  dilutedFactor: number = 0.75
+): void {
+  teams.forEach((row) => {
+    const scores = convertTeamRowToGameScores(row);
+    DAYS.forEach((day, i) => {
+      const oldScore = row[day]?.score;
+      if (!oldScore) return;
+      // test if the current day is the 2d back-to-back game
+      if (isBackToBack(scores, i)) {
+        // multiply by 0.75
+        row[day]!.score = oldScore * dilutedFactor;
+      }
+      // test if the opponent is also playing a 2d back-to-back game
+      const opponent = teams.find((item) => {
+        return item.teamName === row[day]?.opponentName;
+      });
+
+      if (opponent) {
+        const opponentGameScores = convertTeamRowToGameScores(opponent);
+        if (isBackToBack(scores, i) && isBackToBack(opponentGameScores, i)) {
+          // don't multiply by 0.75
+          row[day]!.score = oldScore;
+        }
+      }
+    });
+  });
+}
+
 /**
  * GameScore would have to be adjusted to be within a -5 to 5 range, with 50% being 0.
   - 75% winOdds = 2.5
